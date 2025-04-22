8b9ce2060fdaec84855651efedfbf7bbcffddbc5
Mohammad Mahdi <mohammadmahdi@Mohammads-MacBook-Pro-6.local>
2025-04-22 10:29:51 +0330

    add user models

---
+++ users/models.py
@@ -0,0 +1,37 @@
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+
+class UserManager(BaseUserManager):
+    def create_user(self, mobile, password=None, **extra_fields):
+        if not mobile:
+            raise ValueError('Mobile number is required')
+        user = self.model(mobile=mobile, **extra_fields)
+        if password:
+            user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, mobile, password, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        return self.create_user(mobile, password, **extra_fields)
+
+class User(AbstractBaseUser):
+    mobile = models.CharField(max_length=15, unique=True)
+    full_name = models.CharField(max_length=100, blank=True)
+    email = models.EmailField(blank=True)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    USERNAME_FIELD = 'mobile'
+    REQUIRED_FIELDS = []
+
+    objects = UserManager()
+
+class OneTimeCode(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    code = models.CharField(max_length=6)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user.mobile} - {self.code}"
